14d0a9279abc58c71f097a32d116593ba0d35e4d
Simone Taeggi <startae14@gmail.com>
2020-01-06 23:37:25 +0100

    Use the debug method of the react testing library

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,23 +1,10 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
-// import {getQueriesForElement} from '@testing-library/dom'
 import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
-// create a custom render
-/*
-function render(ui) {
-  const container = document.createElement('div')
-  ReactDOM.render(ui, container)
-  const queries = getQueriesForElement(container)
-  return { container, ...queries }
-}
-*/
-
 test('renders a number input with a label "Favorite Number"', () => {
-  const {getByLabelText} = render(<FavoriteNumber />)
-  // get input by label value
+  const {getByLabelText, debug} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
-  // check if the input is a number
   expect(input).toHaveAttribute('type', 'number')
+  debug(input)
 })
