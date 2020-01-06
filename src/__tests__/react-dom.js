cf02058f9b297913e295e6fd69e172bdd9287362
Simone Taeggi <startae14@gmail.com>
2020-01-07 00:18:20 +0100

    use the queryByRole to check if an element is not rendered

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -10,10 +10,14 @@ test('renders a number input with a label "Favorite Number"', () => {
 })
 
 test('rendering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole, rerender} = render(<FavoriteNumber />)
+  const {getByLabelText, getByRole, queryByRole, rerender} = render(
+    <FavoriteNumber />,
+  )
   const input = getByLabelText(/favorite number/i)
   //fireEvent.change(input, {target: {value: '10'}})
   user.type(input, '10')
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
   rerender(<FavoriteNumber max={10} />)
+  // now check if there is not error
+  expect(queryByRole('alert')).toBeNull()
 })
