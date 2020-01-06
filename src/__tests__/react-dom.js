55d5f20186d2786cf7638b55c1656c9aaf2edbfc
Simone Taeggi <startae14@gmail.com>
2020-01-06 23:28:40 +0100

    use the render of the react-testing-library

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {getQueriesForElement} from '@testing-library/dom'
+// import ReactDOM from 'react-dom'
+// import {getQueriesForElement} from '@testing-library/dom'
+import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
+// create a custom render
+/*
+function render(ui) {
+  const container = document.createElement('div')
+  ReactDOM.render(ui, container)
+  const queries = getQueriesForElement(container)
+  return { container, ...queries }
+}
+*/
+
 test('renders a number input with a label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  const {getByLabelText} = getQueriesForElement(div)
+  const {getByLabelText} = render(<FavoriteNumber />)
   // get input by label value
   const input = getByLabelText(/favorite number/i)
   // check if the input is a number
