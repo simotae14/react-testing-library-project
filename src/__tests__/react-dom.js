c34113a78d091aea529f1c84389d1997db4a23b4
Simone Taeggi <startae14@gmail.com>
2020-01-06 23:54:50 +0100

    use the fireEvent to trigger events on Dom elements

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
-  const {getByLabelText, debug} = render(<FavoriteNumber />)
+  const {getByLabelText} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
-  debug(input)
+})
+
+test('rendering an invalid value shows an error message', () => {
+  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
+  const input = getByLabelText(/favorite number/i)
+  fireEvent.change(input, {target: {value: '10'}})
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
 })
